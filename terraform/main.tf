1043620aa06412e2023ca0dd5a2e3b1a93f03f77
Ridowan Sikder <ridowan.cse@gmail.com>
2025-12-20 23:41:13 +0600

    ci/cd fix : fixed subent and secuirty group region

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,12 +47,9 @@ resource "aws_default_route_table" "cicd-tf-rt" {
   }
 }
 
-resource "aws_vpc" "mainvpc" {
-  cidr_block = "10.1.0.0/16"
-}
 
 resource "aws_default_security_group" "cicd-tf-sg" {
-  vpc_id = aws_vpc.mainvpc.id
+  vpc_id = aws_vpc.cicd-tf-vpc.id
 
   ingress {
     protocol  = "TCP"
